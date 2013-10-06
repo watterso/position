86dd63322240337eca87404c982ed9db6dab66c4
James Watterson <jameslwatterson@gmail.com>
2013-10-06 05:10:48 -0400

    need UI

--- workspace/Position/src/com/watterso/position/DataIntentService.java
+++ workspace/Position/src/com/watterso/position/DataIntentService.java
@@ -1,10 +1,22 @@
 package com.watterso.position;
 
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.IntentService;
 import android.content.Context;
@@ -15,12 +27,30 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
-public class DataIntentService extends IntentService implements SensorEventListener {
+public class DataIntentService extends IntentService implements
+		SensorEventListener {
+	public class DeltaSnapShot {
+		public float x;
+		public float y;
+		public long time;
+
+		public DeltaSnapShot(float lx, float ly, long t) {
+			x = lx;
+			y = ly;
+			time = t;
+		}
+	}
+
+	public static final String MONGO_API = "jIHkv3sxi0kID-9TRpmg_5Yckd2dKF7M";
+	public static final String MONGO_URL = "https://api.mongolab.com/api/1/databases/positioning/collections/wifi?apiKey="
+			+ MONGO_API;
 	private static final int MAX_ACCEL = 50;
 	private static final int MAX_WIFI = 500;
 	private final IBinder mBinder = new DataBinder();
 	private Intent mReceivedIntent;
+	private MainActivity mMainActivity;
 	// Sensors
 	private SensorManager mSensorManager;
 	private Sensor mLinearAcceleration;
@@ -30,30 +60,50 @@ public class DataIntentService extends IntentService implements SensorEventListe
 	private float[] mGravity;
 	private float[] mGeoMagnetic;
 	private float[] mLinearAcc;
-	private HashMap<Long, Float[]> mAData;
+	private HashMap<String, DeltaSnapShot[]> mSnaps; // 0: increase, 1: decrease
+	private HashMap<Long, float[]> mAData;
 	private HashMap<Long, HashMap<String, Integer>> mWData;
+	private HashMap<String, Integer> mRecentWifiAverages;
 	private PriorityQueue<Long> mWTimeStamps;
-	public DataIntentService(){
+	private PriorityQueue<Long> mATimeStamps;
+
+	public DataIntentService() {
 		super("WIFIVEL");
 
 		mGravity = new float[3];
 		mGeoMagnetic = new float[3];
 		mLinearAcc = new float[3];
-		
-		mAData = new HashMap<Long, Float[]>();
-		mWData = new HashMap<Long, HashMap<String,Integer>>();
+
+		mAData = new HashMap<Long, float[]>();
+		mWData = new HashMap<Long, HashMap<String, Integer>>();
+		mSnaps = new HashMap<String, DataIntentService.DeltaSnapShot[]>();
+		mRecentWifiAverages = new HashMap<String, Integer>();
 		mWTimeStamps = new PriorityQueue<Long>();
+		mATimeStamps = new PriorityQueue<Long>();
 	}
-	public void setupSensors(Context c){
-		mSensorManager = (SensorManager) c.getSystemService(Context.SENSOR_SERVICE);
-		mLinearAcceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-		
-		mSensorManager.registerListener(this, mLinearAcceleration, SensorManager.SENSOR_DELAY_NORMAL);
-		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-		mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+
+	public void setupSensors(Context c) {
+		mSensorManager = (SensorManager) c
+				.getSystemService(Context.SENSOR_SERVICE);
+		mLinearAcceleration = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mMagneticField = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+		mSensorManager.registerListener(this, mLinearAcceleration,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mMagneticField,
+				SensorManager.SENSOR_DELAY_NORMAL);
 	}
+
+	public void setMainActivity(MainActivity m) {
+		mMainActivity = m;
+	}
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
@@ -81,7 +131,7 @@ public class DataIntentService extends IntentService implements SensorEventListe
 		}
 		return oot;
 	}
-	
+
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		switch (event.sensor.getType()) {
@@ -101,9 +151,9 @@ public class DataIntentService extends IntentService implements SensorEventListe
 			// filter small changes
 			if (Math.abs(mLinearAcc[0]) > .1 || Math.abs(mLinearAcc[1]) > .1
 					|| Math.abs(mLinearAcc[2]) > .1) {
-				Float[] rotatedAcc = new Float[3];
-				rotatedAcc = rotateVector(mLinearAcc, rotation);
-				testAccellerometerSize(event.timestamp, rotatedAcc);
+				// Float[] rotatedAcc = new Float[3];
+				// rotatedAcc = rotateVector(mLinearAcc, rotation);
+				testAccellerometerSize(event.timestamp, mLinearAcc);
 				// Log.d("ROTATE", Arrays.toString(rotation));
 				// Log.d("LINEAR", Arrays.toString(mLinearAcc));
 				// Log.d("YOLO",
@@ -112,94 +162,198 @@ public class DataIntentService extends IntentService implements SensorEventListe
 		}
 	}
 
-	public HashMap<Long, Float[]> getAccelerometerData(){
+	public HashMap<Long, float[]> getAccelerometerData() {
 		return mAData;
 	}
-	public HashMap<Long, HashMap<String, Integer>> getWifiData(){
+
+	public HashMap<Long, HashMap<String, Integer>> getWifiData() {
 		return mWData;
 	}
-	
+
+	HashMap<String, Integer> getWifiAverageData() {
+		return mRecentWifiAverages;
+	}
+
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
-	
+
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		
+
 	}
-	public void pushWifiData(Long time,HashMap<String, Integer> snapshot) {
+
+	public void pushWifiData(Long time, HashMap<String, Integer> snapshot) {
 		mWData.put(time, snapshot);
 		mWTimeStamps.offer(time);
 		testWifiSize();
 	}
-	private void testWifiSize(){
-		if(mWTimeStamps.size()>MAX_WIFI){
+
+	private void testWifiSize() {
+		if (mWTimeStamps.size() > MAX_WIFI) {
 			Long pop = mWTimeStamps.poll();
 			mWData.remove(pop);
 		}
 	}
-	private void testAccellerometerSize(long timestamp, Float[] accData){
-		if(mAData.size()>=MAX_ACCEL){
+
+	private void testAccellerometerSize(long timestamp, float[] accData) {
+		if (mAData.size() >= MAX_ACCEL) {
 			calcVelocity();
 			mAData.clear();
+			mATimeStamps.clear();
 		}
 		mAData.put(timestamp, accData);
+		mATimeStamps.offer(timestamp);
 	}
-	
+
 	private void calcVelocity() {
+		float x = 0;
+		float y = 0;
+		for (int i = 0; i < mAData.size() - 1; i++) {
+			Long pop = mATimeStamps.poll();
+			Long next = mATimeStamps.peek();
+			float[] arr = mAData.get(pop);
+			float[] arr1 = mAData.get(next);
+			x += adaptive(arr[0], arr1[0]);
+			y += adaptive(arr[1], arr1[1]);
+		}
+
 		Long[] times = new Long[1];
 		times = mAData.keySet().toArray(times);
 		Arrays.sort(times);
 		Long t0 = times[0];
-		Long tf = times[times.length-1];
-		int cnt = 0;
-		HashMap<String,Integer> avgStrength = new HashMap<String, Integer>();
-		for(Long timeStamp : mWData.keySet()){
-			if(timeStamp>t0 && timeStamp<tf){
-				HashMap<String,Integer> lTmp  = mWData.get(timeStamp);
-				for(String bssid : lTmp.keySet()){
-					avgStrength.put(bssid, avgStrength.get(bssid)+lTmp.get(bssid));
+		Long tf = times[times.length - 1];
+		HashMap<String, Integer> avgStrength = new HashMap<String, Integer>();
+		HashMap<String, ArrayList<Integer>> tot = new HashMap<String, ArrayList<Integer>>();
+		for (Long timeStamp : mWData.keySet()) {
+			if (timeStamp >= t0 && timeStamp <= tf) {
+				HashMap<String, Integer> lTmp = mWData.get(timeStamp);
+				for (String bssid : lTmp.keySet()) {
+					if (tot.get(bssid) != null) {
+						tot.get(bssid).add(lTmp.get(bssid));
+					}else{
+						ArrayList<Integer> tmp = new ArrayList<Integer>();
+						tmp.add(lTmp.get(bssid));
+						tot.put(bssid, tmp);
+					}
 				}
-				cnt++;
 			}
 		}
-		for(String bssid : avgStrength.keySet()){
-			avgStrength.put(bssid, (int)(avgStrength.get(bssid)/cnt+.5));	//avg rounded
+		for (String bssid : tot.keySet()) {
+			ArrayList<Integer> strengths = tot.get(bssid);
+			Integer level = 0;
+			for (Integer i : strengths) {
+				level += i;
+			}
+			if(level == 0 || strengths.size()==0) 
+				return;
+			Collections.sort(strengths);
+			Integer mean = (int) (level / strengths.size() + .5);
+			Log.d("SIZE&MEAN", "SIZE: "+strengths.size()+" MEAN: "+mean);
+			Integer median = strengths.get(strengths.size()/2);
+			if(mSnaps.get(bssid) == null){
+				mSnaps.put(bssid,new DeltaSnapShot[2]);
+			}
+			if (median > mean) {
+				// growing, index 0
+				mSnaps.get(bssid)[0] = new DeltaSnapShot(x, y, tf);
+			} else {
+				// shrinking, index 1
+				mSnaps.get(bssid)[1] = new DeltaSnapShot(x, y, tf);
+			}
+			avgStrength.put(bssid, mean); // avg rounded
 		}
-		
-		float x = 0;
-		float y = 0;
-		ArrayList<Float []> tmp = (ArrayList<Float []>)mAData.values();
-		for(int i =0; i< tmp.size()-1; i++){
-			Float[] arr = tmp.get(i);
-			Float[] arr1 = tmp.get(i+1);
-			x+=adaptive(arr[0],arr1[0]);
-			y+=adaptive(arr[1],arr1[1]);
+		mRecentWifiAverages = avgStrength;
+		if (mRecentWifiAverages.size() > 0)
+			updateMongo();
+	}
+
+	private void updateMongo() {
+		JSONObject oot = new JSONObject();
+		try {
+			oot.put("device", android.os.Build.MODEL);
+			JSONObject info = new JSONObject();
+			for (String bssid : mRecentWifiAverages.keySet()) {
+				info.put(bssid, mRecentWifiAverages.get(bssid));
+			}
+			oot.put("data", info);
+			mMainActivity.new PutDataTask().execute(oot);
+		} catch (JSONException e) {
+			Log.d("YOLOJSON", e.getMessage());
 		}
 	}
 
-	//TODO: Stop being lazy and do integration on your own
-	//Adapted from:
-	//http://introcs.cs.princeton.edu/java/93integration/AdaptiveQuadrature.java.html
+	// DAT ALGOS
+	public static Integer select(ArrayList<Integer> list, int k) {
+		if(list.size()==1){
+			return list.get(0);
+		}
+		Integer med = medianOfMedians(list);
+		list.remove(med);
+
+		ArrayList<Integer> greater = new ArrayList<Integer>();
+		ArrayList<Integer> less = new ArrayList<Integer>();
+		for (Integer i : list) {
+			if (i <= med) {
+				less.add(i);
+			} else {
+				greater.add(i);
+			}
+		}
+		if (less.size() + 1 == k) {
+			return med;
+		} else if (less.size() + 1 < k) {
+			return select(less, k);
+		} else {
+			return select(greater, k - (less.size() + 1));
+		}
+	}
+
+	public static Integer medianOfMedians(ArrayList<Integer> list) {
+		ArrayList<Integer> medians = new ArrayList<Integer>();
+		if(list.size()==0)
+			return 0;
+		if(list.size()<5){
+			return list.get((int)(list.size()/2));
+		}
+		for (int i = 0; ((i+1)*5-1) < list.size(); i++) {
+			Integer[] subArr = new Integer[1];
+			Log.d("YOLO", ((i+1)*5-1)+" ? "+list.size());
+			subArr = list.subList(i, (i + 1) * 5 - 1).toArray(subArr);
+			Arrays.sort(subArr);
+			medians.add(subArr[2]);
+		}
+		Integer[] subArr = new Integer[1];
+		subArr = medians.toArray(subArr);
+		Arrays.sort(subArr);
+		return subArr[subArr.length / 2];
+
+	}
+
+	// TODO: Stop being lazy and do integration on your own
+	// Adapted from:
+	// http://introcs.cs.princeton.edu/java/93integration/AdaptiveQuadrature.java.html
 	private final static double EPSILON = 1E-6;
+
 	// adaptive quadrature
-    public static float adaptive(float a, float b) {
-        float h = b - a;
-        float c = (float) ((a + b) / 2.0);
-        float d = (float) ((a + c) / 2.0);
-        float e = (float) ((b + c) / 2.0);
-        float Q1 = h/6  * (f(a) + 4*f(c) + f(b));
-        float Q2 = h/12 * (f(a) + 4*f(d) + 2*f(c) + 4*f(e) + f(b));
-        if (Math.abs(Q2 - Q1) <= EPSILON)
-            return Q2 + (Q2 - Q1) / 15;
-        else
-            return adaptive(a, c) + adaptive(c, b);
-    }
-    /**********************************************************************
-     * Standard normal distribution density function.
-     * Replace with any sufficiently smooth function.
-     **********************************************************************/
-     static float f(float x) {
-         return (float) (Math.exp(- x * x / 2) / Math.sqrt(2 * Math.PI));
-     }
+	public static float adaptive(float a, float b) {
+		float h = b - a;
+		float c = (float) ((a + b) / 2.0);
+		float d = (float) ((a + c) / 2.0);
+		float e = (float) ((b + c) / 2.0);
+		float Q1 = h / 6 * (f(a) + 4 * f(c) + f(b));
+		float Q2 = h / 12 * (f(a) + 4 * f(d) + 2 * f(c) + 4 * f(e) + f(b));
+		if (Math.abs(Q2 - Q1) <= EPSILON)
+			return Q2 + (Q2 - Q1) / 15;
+		else
+			return adaptive(a, c) + adaptive(c, b);
+	}
+
+	/**********************************************************************
+	 * Standard normal distribution density function. Replace with any
+	 * sufficiently smooth function.
+	 **********************************************************************/
+	static float f(float x) {
+		return (float) (Math.exp(-x * x / 2) / Math.sqrt(2 * Math.PI));
+	}
 }
--- workspace/Position/src/com/watterso/position/MainActivity.java
+++ workspace/Position/src/com/watterso/position/MainActivity.java
@@ -2,11 +2,14 @@ package com.watterso.position;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -39,13 +42,13 @@ import com.watterso.position.DataIntentService.DataBinder;
 public class MainActivity extends Activity implements SensorEventListener {
 	private DataIntentService mDataIntentService;
 	private boolean ScanAsFastAsPossible = true;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Intent intent = new Intent(this, DataIntentService.class);
-		if(bindService(intent,mServiceConnection,Context.BIND_AUTO_CREATE)){
+		if (bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)) {
 			Log.d("YOLO", "Bound Success!");
 		}
 		IntentFilter i = new IntentFilter();
@@ -56,12 +59,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 						.getAction()))
 					return;
 				WifiManager w = (WifiManager) c.getSystemService(Context.WIFI_SERVICE);
-				Long time = System.nanoTime();
-				HashMap<String,Integer> snapshot = new HashMap<String, Integer>();
-				for(ScanResult scanRes : w.getScanResults()){
+				Long time = System.currentTimeMillis()*1000000;
+				HashMap<String, Integer> snapshot = new HashMap<String, Integer>();
+				for (ScanResult scanRes : w.getScanResults()) {
 					snapshot.put(scanRes.BSSID, scanRes.level);
 				}
-				mDataIntentService.pushWifiData(time,snapshot);
+				mDataIntentService.pushWifiData(time, snapshot);
 				if (ScanAsFastAsPossible)
 					w.startScan();
 				else {
@@ -69,9 +72,9 @@ public class MainActivity extends Activity implements SensorEventListener {
 				}
 			}
 		}, i);
-		Button post = (Button)findViewById(R.id.postButton);
+		Button post = (Button) findViewById(R.id.postButton);
 		post.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				postData();
@@ -87,13 +90,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 	@Override
 	protected void onPause() {
 		super.onPause();
-		
+
 	}
-	
+
 	@Override
 	protected void onStop() {
 		super.onStop();
-		
+
 	}
 
 	@Override
@@ -104,64 +107,91 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		
+
 	}
-	private void postData(){
-		new PostDataTask().execute(getJsonFromVectorMap(mDataIntentService.getAccelerometerData()));
-		//new PostDataTask().execute(getJsonFromWifiMap(mDataIntentService.getWifiData()));
+
+	private void postData() {
+		// new
+		// PostDataTask().execute(getJsonFromVectorMap(mDataIntentService.getAccelerometerData()));
+		// new
+		// PostDataTask().execute(getJsonFromWifiMap(mDataIntentService.getWifiData()));
+		new PostDataTask().execute(getJsonFromWifiAverageMap(mDataIntentService
+				.getWifiAverageData()));
 	}
-	
+
+	public static JSONObject getJsonFromWifiAverageMap(
+			HashMap<String, Integer> wifiAverageData) {
+		JSONObject info = new JSONObject();
+		for(String bssid : wifiAverageData.keySet()){
+			try {
+				info.put(bssid, wifiAverageData.get(bssid));
+			} catch (JSONException e) {
+				Log.d("YOLOJSON", e.getMessage());
+			}
+		}
+		return info;
+	}
+
+	public void putData(JSONObject json) {
+		new PutDataTask().execute(json);
+	}
+
 	private ServiceConnection mServiceConnection = new ServiceConnection() {
-		
+
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
-			DataBinder binder = (DataBinder)service;
-			if(mDataIntentService == null){
+			DataBinder binder = (DataBinder) service;
+			if (mDataIntentService == null) {
 				mDataIntentService = binder.getService();
 				mDataIntentService.setupSensors(getApplicationContext());
+				mDataIntentService.setMainActivity(MainActivity.this);
 			}
-			Log.d("YOLO", "IS NULL? "+(mDataIntentService == null));
+			Log.d("YOLO", "IS NULL? " + (mDataIntentService == null));
 		}
 	};
-	public static JSONObject getJsonFromWifiMap(HashMap<Long, HashMap<String,Integer>> in ){
+
+	public static JSONObject getJsonFromWifiMap(
+			HashMap<Long, HashMap<String, Integer>> in) {
 		JSONObject oot = new JSONObject();
-		for(Long l : in.keySet()){
+		for (Long l : in.keySet()) {
 			try {
-				HashMap<String,Integer> tmp = in.get(l);
+				HashMap<String, Integer> tmp = in.get(l);
 				JSONObject snap = new JSONObject();
-				for(String s : tmp.keySet()){
+				for (String s : tmp.keySet()) {
 					snap.put(s, tmp.get(s));
 				}
-				oot.put(""+l, snap);
+				oot.put("" + l, snap);
 			} catch (JSONException e) {
 				Log.d("YOLOJSON", e.getMessage());
 			}
 		}
 		return oot;
 	}
-	public static JSONObject getJsonFromVectorMap(HashMap<Long,Float[]> in){
+
+	public static JSONObject getJsonFromVectorMap(HashMap<Long, float[]> in) {
 		JSONObject oot = new JSONObject();
-		for(Long l : in.keySet()){
+		for (Long l : in.keySet()) {
 			try {
-				Float[] arr1 = in.get(l);
+				float[] arr1 = in.get(l);
 				JSONArray arr = new JSONArray();
 				arr.put(arr1[0]);
 				arr.put(arr1[1]);
 				arr.put(arr1[2]);
-				oot.put(""+l, arr);
+				oot.put("" + l, arr);
 			} catch (JSONException e) {
 				Log.d("YOLOJSON", e.getMessage());
 			}
 		}
 		return oot;
 	}
-	private class PostDataTask extends AsyncTask<JSONObject,Void,Void>{
+
+	private class PostDataTask extends AsyncTask<JSONObject, Void, Void> {
 
 		@Override
 		protected Void doInBackground(JSONObject... params) {
@@ -174,17 +204,43 @@ public class MainActivity extends Activity implements SensorEventListener {
 				Log.d("YOLOENC", e.getMessage());
 			}
 			lePost.setHeader("Accept", "application/json");
-		    lePost.setHeader("Content-type", "application/json");
-		    DefaultHttpClient httpclient = new DefaultHttpClient();
-		    ResponseHandler responseHandler = new BasicResponseHandler();
-		    try {
+			lePost.setHeader("Content-type", "application/json");
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			ResponseHandler responseHandler = new BasicResponseHandler();
+			try {
 				httpclient.execute(lePost, responseHandler);
 			} catch (ClientProtocolException e) {
 				Log.d("YOLOPROTO", e.getMessage());
 			} catch (IOException e) {
 				Log.d("YOLOIO", e.getMessage());
 			}
-		    Log.d("INFO", "POSTED");
+			Log.d("INFO", "POSTED");
+			return null;
+		}
+	}
+
+	public class PutDataTask extends AsyncTask<JSONObject, Void, Void> {
+
+		@Override
+		protected Void doInBackground(JSONObject... params) {
+			JSONObject json = params[0];
+			try {
+				String url = DataIntentService.MONGO_URL
+						+ "&q="
+						+ URLEncoder.encode("{\"device\":\""
+								+ android.os.Build.MODEL + "\"}", "UTF-8")
+						+ "&u=true";
+				HttpPut httpPut = new HttpPut(url);
+				httpPut.setHeader("Content-type", "application/json");
+				httpPut.setEntity(new StringEntity(json.toString()));
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				HttpResponse response = httpClient.execute(httpPut);
+			} catch (ClientProtocolException e) {
+				Log.d("YOLOPROTO", e.getMessage());
+			} catch (IOException e) {
+				Log.d("YOLOIO", e.getMessage());
+			}
+			Log.d("INFO", "POSTED");
 			return null;
 		}
 	}
